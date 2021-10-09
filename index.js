89c818b200a44da1d781011d0d3471dc519dd983
Natnael Meseret <natnael0meseret@gmail.com>
2021-10-10 00:29:23 +0300

    Removed unnecessarily third party dependencies and used fs watch with tail. More efficient and doesn't crash on file deletes.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import fs from 'fs';
 import path from 'path';
-import Tail from 'tail';
+// import Tail from 'tail';
+// import chokidar from 'chokidar'
 import express from'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors'
+import { spawn } from 'child_process';
 
 if (!Array.prototype.last){
     Array.prototype.last = function(){
@@ -27,97 +29,6 @@ var meta = {}
 // Serve static files (i.e front-end build)
 console.log("Serving static assets...");
 app.use(express.static('public'));
-
-// // Socket events
-// io.of("/containers").on("connection", (socket) => {
-//     socket.on('meta', () => {
-//         console.log("Sending meta ...");
-//         socket.emit("meta", meta);
-//     });
-
-//     socket.on("subscribe", (subscription)=>{
-//         console.log("Subscribing to ...", subscription.containerID);
-
-//         fs.readFile(meta[subscription.containerID].logFileName, 'utf8', function(err, data){
-//             if(data.split('\n').length > 50)
-//                 socket.emit(subscription.containerID+'-init', data.split('\n').slice(data.split('\n').length - 50));
-//             else 
-//                 socket.emit(subscription.containerID+'-init', data.split('\n'));
-            
-//             socket.join(subscription.containerID);
-
-//             // let tail = new Tail.Tail(meta[subscription.containerID].logFileName);
-    
-//             // tail.on("line", (line) => {
-//             //     socket.emit("line", JSON.parse(line).log);
-//             // });
-
-//             // socket.on("unsubscribe", () => {
-//             //     console.log("Unsubscribing...", meta[subscription.containerID].logFileName);
-//             //     tail.unwatch();
-//             // });
-//         });
-//     });
-
-//     socket.on("unsubscribe", (data) => {
-//         console.log("Unsubscribing from ..", data.containerID);
-//         socket.leave(data.containerID);
-//         socket.emit('unsubscribed', data.containerID);
-//     });
-// });
-
-
-// function main() {
-//     const pathToContainers = path.join('/', 'var', 'lib', 'docker', 'containers'); // /var/lib/docker/containers/
-//     const configFileName = 'hostconfig.json';
-
-//     var directories = fs.readdirSync( pathToContainers );
-
-//     directories.forEach( containerDirectory => {
-//         const containerID = containerDirectory;
-//         const containerName = JSON.parse( fs.readFileSync(path.join(pathToContainers, containerID, configFileName) ).toString() ).Binds[0].split(":")[0].split("/").last();
-//         var fname = path.join(pathToContainers, containerID, containerID+'-json.log');
-
-//         meta[containerID] = {
-//             id: containerID,
-//             name: containerName,
-//             logFileName: fname
-//         }
-
-//         let tail = new Tail.Tail(fname);
-
-//         tail.on("line", (line) => {
-//             io.of('/containers').to(containerID).emit(containerID+'-line', JSON.parse(line).log);
-//         });
-//     });
-
-//     server.listen(port, () => {
-//         console.log(`Log server running at: ${port}`);
-//     });
-// }
-// main();
-
-app.get('/meta', function (req, res) {
-    var metaInfo = {};
-    const pathToContainers = path.join('/', 'var', 'lib', 'docker', 'containers'); // /var/lib/docker/containers/
-    const configFileName = 'hostconfig.json';
-
-    var directories = fs.readdirSync( pathToContainers );
-
-    directories.forEach( containerDirectory => {
-        const containerID = containerDirectory;
-        const containerName = JSON.parse( fs.readFileSync(path.join(pathToContainers, containerID, configFileName) ).toString() ).Binds[0].split(":")[0].split("/").last();
-        var fname = path.join(pathToContainers, containerID, containerID+'-json.log');
-
-        metaInfo[containerID] = {
-            id: containerID,
-            name: containerName,
-            logFileName: fname
-        }
-    });
-    
-    res.status(200).send(metaInfo)
-});
 
 server.listen(port, () => {
     const pathToContainers = path.join('/', 'var', 'lib', 'docker', 'containers'); // /var/lib/docker/containers/
@@ -145,34 +56,67 @@ server.listen(port, () => {
                 else 
                     socket.emit(containerID+'-init', data.split('\n'));
 
-                try {
-                    var tail = new Tail.Tail(fname);
-
-                    tail.on("line", (line) => {
-                        socket.emit(containerID+'-line', JSON.parse(line).log);
-                        // io.of('/containers').to(containerID).emit(containerID+'-line', JSON.parse(line).log);
+                var tail = spawn('tail', ['-n', 0, '-f', fname]);
+                tail.stdout.on('data', (data) => {
+                    data.toString().trim().split('\n').forEach(line => {
+                        socket.emit(containerID+'-line', JSON.parse(line.toString('utf-8')).log);
                     });
-
-                    tail.on("error", (err) => {
-                        socket.emit(containerID+'-error', err);
-                        socket.disconnect();
-                    });
-                } catch (err) {
-                    socket.emit(containerID+'-error', err);
-                    socket.disconnect();
-                    return
-                }
-            });
-
-            socket.on("unsubscribe", (data) => {
-                // console.log("Unsubscribing from ..", data.containerID);
-                // socket.leave(data.containerID);
-                // socket.emit('unsubscribed', data.containerID);
-                socket.disconnect();
+                });
             });
         });
-
     });
+
+    fs.watch(pathToContainers, (eventType, filename) => {
+        if(eventType == 'rename') {
+            var containerID = filename;
+            if(meta[containerID]) {
+                delete meta[containerID];
+                
+                io.of('/meta').sockets.forEach(socket => {
+                    socket.emit('removed', containerID);
+                });
+            } else {
+                setTimeout(()=>{ // Wait till all files are initialized and follow the log tail
+                    var containerName = JSON.parse( fs.readFileSync(path.join(pathToContainers, containerID, configFileName) ).toString() ).Binds[0].split(":")[0].split("/").last();
+                    var fname = path.join(pathToContainers, containerID, containerID+'-json.log');
+                    
+                    meta[containerID] = {
+                        id: containerID,
+                        name: containerName,
+                        logFileName: fname
+                    }
+
+                    // Socket events
+                    io.of("/"+containerID).on("connection", (socket) => {
+                        console.log("Connected ...");
+                        fs.readFile(fname, 'utf8', function(err, data){
+                            if(data.split('\n').length > 50)
+                                socket.emit(containerID+'-init', data.split('\n').slice(data.split('\n').length - 50));
+                            else 
+                                socket.emit(containerID+'-init', data.split('\n'));
     
+                            var tail = spawn('tail', ['-n', 0, '-f', fname]);
+                            tail.stdout.on('data', (data) => {
+                                data.toString().trim().split('\n').forEach(line => {
+                                    socket.emit(containerID+'-line', JSON.parse(line.toString('utf-8')).log);
+                                });
+                            });
+                        });
+                    });
+
+                    // Send container configs meta subscribers
+                    io.of('/meta').sockets.forEach(socket => {
+                        socket.emit('added', meta[containerID]);
+                    });
+                }, 5000);
+            }
+        }
+    });
+
+    // Socket events
+    io.of("/meta").on("connection", (socket) => {
+        socket.emit("meta", meta);
+    });
+
     console.log(`Log server running at: ${port}`);
 });
