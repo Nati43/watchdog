45a9c940d7f99fab4d106d9ffedd863ed66907d8
Natnael Meseret <natnael0meseret@gmail.com>
2021-10-12 01:51:47 +0300

    Made some performance and stability tweaks (Using single sockets, Watching on demand only and closing on unsubscribe/disconnect, emitting/down/removed/added event on single socket). Added latest front-end build.

--- index.js
+++ index.js
@@ -24,15 +24,14 @@ const io = new Server(server, {
 
 var meta = {}
 
-// Serve static files (i.e front-end build)
-console.log("Serving static assets...");
-app.use(express.static('public'));
-
-server.listen(port, () => {
+// Serve meta content
+io.of("/meta").on("connection", (socket) => {
     const pathToContainers = path.join('/', 'var', 'lib', 'docker', 'containers'); // /var/lib/docker/containers/
     const configFileName = 'config.v2.json';
 
     var directories = fs.readdirSync( pathToContainers );
+
+    var configWatchers = {};
 
     directories.forEach( containerDirectory => {
         const containerID = containerDirectory;
@@ -47,55 +46,70 @@ server.listen(port, () => {
             running: containerState
         }
 
-        chokidar
-        .watch(path.join(pathToContainers, containerID, configFileName))
-        .on('change', () => {
+        // Watch container status
+        var configWatcher = chokidar.watch(path.join(pathToContainers, containerID, configFileName));
+        configWatcher.on('change', () => {
             fs.readFile(path.join(pathToContainers, containerID, configFileName), {}, (err, data)=> {
                 if(!err) {
                     var running = JSON.parse(data).State.Running;
-                    if(meta[containerID].running && !running) {
-                        console.log("Emitting down ...!");
-                        io.of('/meta').sockets.forEach(socket => {
-                            socket.emit('down', containerID);
-                        });
-                    }else if(!meta[containerID].running && running) {
-                        console.log("Emitting up ...!");
-                        io.of('/meta').sockets.forEach(socket => {
-                            socket.emit('up', containerID);
-                        });
+                    if(meta[containerID] != undefined && meta[containerID].running && !running) {
+                        meta[containerID].running = false;
+                        socket.emit('down', containerID);
+                    }else if(meta[containerID] != undefined && !meta[containerID].running && running) {
+                        meta[containerID].running = true;
+                        socket.emit('up', containerID);
                     }
                 }
             });
         });
+        configWatchers[containerID] = configWatcher;
 
-        // Socket events
-        io.of("/"+containerID).on("connection", (socket) => {
-            fs.readFile(fname, 'utf8', function(err, data){
+        // Event: Subscribe to log file
+        socket.on(containerID+'-subscribe', () => {
+            socket.emit(containerID+'-subscribed');
+            fs.readFile(fname, 'utf8', function(err, data) {
                 if(data.split('\n').length > 50)
                     socket.emit(containerID+'-init', data.split('\n').slice(data.split('\n').length - 50));
                 else 
                     socket.emit(containerID+'-init', data.split('\n'));
 
+                // Tail log file
                 var tail = spawn('tail', ['-n', 0, '-f', fname]);
                 tail.stdout.on('data', (data) => {
                     data.toString().trim().split('\n').forEach(line => {
                         socket.emit(containerID+'-line', JSON.parse(line.toString('utf-8')).log);
                     });
                 });
+
+                // Close tail on disconnect (container)
+                socket.on(containerID+'-unsubscribe', () => {
+                    tail.kill();
+                    socket.emit(containerID+'-unsubscribed');
+                });
+
+                // Close tail on disconnect (socket)
+                socket.on('disconnect', ()=> {
+                    tail.kill();
+                });
             });
         });
+
     });
 
-    // Detect when containers are removed ...
-    fs.watch(pathToContainers, (eventType, filename) => {
+    // Send meta
+    socket.emit("meta", meta);
+
+    // Watch containers to detect when containers are removed/added
+    var containersWatcher = fs.watch(pathToContainers, (eventType, filename) => {
         if(eventType == 'rename') {
             var containerID = filename;
-            if(meta[containerID]) {
+            if(meta[containerID] != undefined) {
                 delete meta[containerID];
-                
-                io.of('/meta').sockets.forEach(socket => {
-                    socket.emit('removed', containerID);
-                });
+                if(configWatchers[containerID] != undefined) {
+                    configWatchers[containerID].close();
+                    delete configWatchers[containerID];
+                }
+                socket.emit('removed', containerID);
             } else {
                 setTimeout(()=>{ // Wait till all files are initialized and follow the log tail
                     var containerName = JSON.parse( fs.readFileSync(path.join(pathToContainers, containerID, configFileName) ).toString() ).Name;
@@ -109,63 +123,83 @@ server.listen(port, () => {
                         running: containerState
                     }
 
-                    chokidar
-                    .watch(path.join(pathToContainers, containerID, configFileName))
-                    .on('change', () => {
+                    // Watch container status
+                    var configWatcher = chokidar.watch(path.join(pathToContainers, containerID, configFileName));
+                    configWatcher.on('change', () => {
                         fs.readFile(path.join(pathToContainers, containerID, configFileName), {}, (err, data)=> {
                             if(!err) {
                                 var running = JSON.parse(data).State.Running;
-                                if(meta[containerID].running && !running) {
-                                    console.log("Emitting down ...!");
-                                    io.of('/meta').sockets.forEach(socket => {
-                                        socket.emit('down', containerID);
-                                    });
-                                }else if(!meta[containerID].running && running) {
-                                    console.log("Emitting up ...!");
-                                    io.of('/meta').sockets.forEach(socket => {
-                                        socket.emit('up', containerID);
-                                    });
+                                if(meta[containerID] != undefined && meta[containerID].running && !running) {
+                                    meta[containerID].running = false;
+                                    socket.emit('down', containerID);
+                                }else if(meta[containerID] != undefined && !meta[containerID].running && running) {
+                                    meta[containerID].running = true;
+                                    socket.emit('up', containerID);
                                 }
                             }
                         });
                     });
+                    configWatchers[containerID] = configWatcher;
 
-                    // Socket events
-                    io.of("/"+containerID).on("connection", (socket) => {
-                        fs.readFile(fname, 'utf8', function(err, data){
+                    // Event: Subscribe to log file
+                    socket.on(containerID+'-subscribe', () => {
+                        socket.emit(containerID+'-subscribed');
+                        fs.readFile(fname, 'utf8', function(err, data) {
                             if(data.split('\n').length > 50)
                                 socket.emit(containerID+'-init', data.split('\n').slice(data.split('\n').length - 50));
                             else 
                                 socket.emit(containerID+'-init', data.split('\n'));
-    
+            
+                            // Tail log file
                             var tail = spawn('tail', ['-n', 0, '-f', fname]);
                             tail.stdout.on('data', (data) => {
                                 data.toString().trim().split('\n').forEach(line => {
                                     socket.emit(containerID+'-line', JSON.parse(line.toString('utf-8')).log);
                                 });
                             });
+
+                            // Close tail on disconnect (container)
+                            socket.on(containerID+'-unsubscribe', () => {
+                                tail.kill();
+                                socket.emit(containerID+'-unsubscribed');
+                            });
+
+                            // Close tail on disconnect (socket)
+                            socket.on('disconnect', ()=> {
+                                tail.kill();
+                            });
                         });
                     });
 
-                    // Send container configs meta subscribers
-                    io.of('/meta').sockets.forEach(socket => {
-                        socket.emit('added', meta[containerID]);
-                    });
+                    // Send container configs
+                    socket.emit('added', meta[containerID]);
                 }, 5000);
             }
         }
     });
 
-    // Serve meta content
-    io.of("/meta").on("connection", (socket) => {
-        socket.emit("meta", meta);
-    }).use(function(socket, next) {
-        if(socket.handshake.query.pin != password) {
-            socket.disconnect();
-        }else{
-            return next();
-        }
+    socket.on('disconnect', () => {
+        // Stop watching containers on disconnect (socket)
+        containersWatcher.close();
+        // Close container watcher on disconnect (socket)
+        Object.values(configWatchers).forEach(configWatcher => {
+            // Close container watcher on disconnect (socket)
+            configWatcher.close();
+        });
     });
 
+}).use(function(socket, next) {
+    if(socket.handshake.query.pin != password) {
+        socket.disconnect();
+    }else{
+        return next();
+    }
+});
+
+// Serve static files (i.e front-end build)
+console.log("Serving static assets...");
+app.use(express.static('public'));
+
+server.listen(port, () => {
     console.log(`Log server running at: ${port}`);
 });
